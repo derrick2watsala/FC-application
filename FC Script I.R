6b32250d92ed16def7e9ad90d1548b5479ff814c
Derrick watsala <dwatsal24@gmail.com>
2025-09-17 13:07:40 +0300

    Hey guys lets start afresh

--- FC Script I.R
+++ FC Script I.R
@@ -1,12 +1,1 @@
-install.packages("usethis")
-library(usethis)
-
-Add text 
-
-My name is the Father
-
-What happened to tgghe nations 
-
-Lets start at 1pm tomowwo
-
-Lets meet at 2pm
+Deeleted it all
